52e8664c6c418f837981a79cc9f7d3c3af84e58a
NGRepNN <n.gladkov.repernn@mail.ru>
2020-02-27 11:29:08 +0300

    1 Jurassic Word WORK

---
+++ src/main/java/JurassicWord.java
@@ -0,0 +1,7 @@
+//Jurassic Word
+public class JurassicWord {
+    public static String lunchTime (String scene) {
+        //return "Something is eating something.";
+        return null;
+    }
+}
---
+++ src/test/java/JurassicWordTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class JurassicWordTest {
+    static String scene1 = "_CVvvvVC}>";
+    static String scene2 = "iiiuuVuuiii";
+    static String scene5 = "|||uuVuu|||";
+    @Test
+    public void test1() {
+        assertEquals("A T-Rex is eating a dead dino.", JurassicWord.lunchTime(scene1));
+    }
+    @Test
+    public void test2() {
+        assertEquals("A triceratops is eating flowers.", JurassicWord.lunchTime(scene2));
+    }
+    @Test
+    public void test3() {
+        assertEquals("Something is eating leaves.", JurassicWord.lunchTime(scene5));
+    }
+}
