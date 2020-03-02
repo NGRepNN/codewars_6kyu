c03f74e0cbf0331e57dd80aca43d1fe259755a03
NGRepNN <n.gladkov.repernn@mail.ru>
2020-03-02 14:10:00 +0300

    4 Mobile Display TEST DONE

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args){
-MobileDisplay.mobileDisplay(36,31);
+MobileDisplay.mobileDisplay(27,41);
     }
 }
--- src/main/java/MobileDisplay.java
+++ src/main/java/MobileDisplay.java
@@ -4,9 +4,15 @@ import java.util.Arrays;
 public class MobileDisplay {
     public static String mobileDisplay(int n, int p){
         int row=(int)((double)(n)*((double)(p)/100))-3;
-        String space="";String rowFirs;String rowEnd;String star=""; String result="";
+        int centrRow=(int)(((double)(n)*((double)(p)/100))/2);
+        int startSpase=(n-10)/2;
+        int endSpace=(n-10)-startSpase;
+        String space="";String rowFirs;String rowEnd;String star=""; String result=""; String MenuContactsSpace=""; String CodeWarsStartSpace="";String CodeWarsEndSpace="";
         for(int i=1;i<=n;i++){
+            if(startSpase>=i){CodeWarsStartSpace=CodeWarsStartSpace+" ";}
+            if(endSpace>=i){CodeWarsEndSpace=CodeWarsEndSpace+" ";}
             if(i>=3) {
+                if(i>16){MenuContactsSpace=MenuContactsSpace+" ";}
                 space = space + " ";
             }
             star=star+"*";
@@ -15,10 +21,13 @@ public class MobileDisplay {
         rowEnd="*\n"+star;
         for(int i=0;i<=row+1;i++){
 if(i==0){result=rowFirs;continue;}
-if(i==row+1){result=result+space+rowEnd;break;}
+if(i==centrRow-1){result=result+CodeWarsStartSpace+"CodeWars"+CodeWarsEndSpace+"*\n*";continue;}
+if(i==row+1){
+    result=result+" Menu"+MenuContactsSpace+"Contacts ";
+    result=result+rowEnd;break;
+}
 result=result+space+"*\n*";
         }
-        System.out.println(result);
-        return "";
+        return result;
     }
 }
