7a74b027f7671166fd913028ae7c202a1044d98b
NGRepNN <n.gladkov.repernn@mail.ru>
2020-03-04 08:00:13 +0300

    11 Order add contains TEST DONE

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -2,12 +2,12 @@ import java.util.ArrayList;
 //Your order, please
 public class Order {
     public static String order(String words) {
-        String result="";
+        String result;
         String [] strArr=words.split(" ");
         ArrayList<String>arrayList=new ArrayList<String>();
         for(int i=0;i<=strArr.length-1;i++){
             for(int j=0;j<=strArr.length-1;j++){
-                if(strArr[j].indexOf(String.valueOf(i+1))>=0){
+                if(strArr[j].contains(String.valueOf(i + 1))){
                     arrayList.add(strArr[j]);
                 }
             }
