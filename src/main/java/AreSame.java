3d3fa0ae7cb2d8c98eee751880790c21fe1485db
NGRepNN <n.gladkov.repernn@mail.ru>
2020-03-04 14:35:26 +0300

    18 Are they the "same"? FIXED TEST DONE

--- src/main/java/AreSame.java
+++ src/main/java/AreSame.java
@@ -3,14 +3,22 @@ import java.util.Arrays;
 //Are they the "same"?
 public class AreSame {
     public static boolean comp(int[] a, int[] b) {
-int [] powArr=new int [a.length];
-int i=0;
-        Arrays.sort(a);
-        Arrays.sort(b);
-for(int j:a){
-    powArr[i]=(int)(Math.pow(j,2));
-    i=i+1;
+        boolean result;
+        if (a.length == 0 || b.length == 0) {
+            result = false;
         }
-        return Arrays.equals(powArr,b);
+        else {
+            int[] powArr = new int[a.length];
+            int i = 0;
+            Arrays.sort(a);
+            Arrays.sort(b);
+            for (int j : a) {
+                powArr[i] = (int) (Math.pow(j, 2));
+                i = i + 1;
+            }
+            result=Arrays.equals(powArr, b);
+        }
+        System.out.println(result);
+        return result;
     }
 }
