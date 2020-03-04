445e50d2b710f43c461a50c009e0911cc4fcb84f
NGRepNN <n.gladkov.repernn@mail.ru>
2020-03-04 11:11:10 +0300

    16 Create Phone Number FIX TEST DONE

--- src/main/java/CreatePhoneNumber.java
+++ src/main/java/CreatePhoneNumber.java
@@ -1,14 +1,16 @@
 //Create Phone Number
 public class CreatePhoneNumber {
     public static String createPhoneNumber(int[] numbers) {
-        String result="(";
+        StringBuilder result= new StringBuilder("(");
         int count=0;
         for(int i:numbers){
-            if(count==3){result=result+") ";}
-            if(count==6){result=result+"-";}
-            result=result+i;
+            if(count==3){
+                result.append(") ");}
+            if(count==6){
+                result.append("-");}
+            result.append(i);
             count+=1;
         }
-        return  result;
+        return result.toString();
     }
 }
