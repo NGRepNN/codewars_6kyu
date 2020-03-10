85dae0ffafcb2710454b638e199d59d2bd6237d4
NGRepNN <n.gladkov.repernn@mail.ru>
2020-03-10 07:48:59 +0300

    23 Convert string to camel case TEST DONE

--- src/main/java/CamelCase.java
+++ src/main/java/CamelCase.java
@@ -3,14 +3,11 @@ import java.lang.StringBuilder;
 public class CamelCase {
     static String toCamelCase(String s){
         StringBuilder sb=new StringBuilder(s);
-        for(int i=0;i<=s.length()-1;i++){
-            if((sb.charAt(0)==Character.toUpperCase(sb.charAt(0)))&&i==0){
-                continue;
-            }
+        for(int i=0;i<=sb.length()-1;i++){
             if(sb.charAt(i)=='-'||sb.charAt(i)=='_'){
-
+sb.deleteCharAt(i);
             }
         }
-        return "";
+        return sb.toString();
     }
 }
